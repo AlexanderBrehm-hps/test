f3dfe4a87c2e638a9fe6d9d697edf557e40700b4
AlexanderBrehm-hps <Alexander.Brehm@e-hps.com>
2025-08-18 16:09:08 -0400

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,10 @@
+using System;
+
+public class Program
+{
+    public void Main()
+    {
+      // Note: this isn't a real class, I just created it to test deployments.
+      Console.WriteLine("Hello world!");
+    }
+}
